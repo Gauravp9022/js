672b20c4bb12f9e476f31ef586eee7066d139e97
Gauravp9022 <165418126+Gauravp9022@users.noreply.github.com>
2024-03-31 12:14:07 +0000

    kk

---
+++ 01_Basic/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(35
+    +
+     30) // code readability should be high
+
+console.log("GAurav")
+
+
+let name = "GAurav"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
