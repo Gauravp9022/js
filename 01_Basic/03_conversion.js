c02d9984ed52804acd0d20c3fcb97996128e4f02
Gauravp9022 <165418126+Gauravp9022@users.noreply.github.com>
2024-03-31 14:04:12 +0000

    done

---
+++ 01_Basic/03_conversion.js
@@ -0,0 +1,32 @@
+let age=22
+console.log(typeof age);
+// output=== number
+
+let score="22"
+console.log(typeof score);
+// output=== string
+
+let valueINnumber=Number (score)
+console.log(typeof valueINnumber)
+console.log(valueINnumber)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+// ""=>0  ;  "Gaurav"=> 1;
+
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Gaurav" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
