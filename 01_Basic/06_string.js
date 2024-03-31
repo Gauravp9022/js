2e512bf0ccbd6a446ff0aedfc910064a738b7153
Gauravp9022 <165418126+Gauravp9022@users.noreply.github.com>
2024-03-31 19:11:47 +0000

    str

---
+++ 01_Basic/06_string.js
@@ -0,0 +1,35 @@
+const name = "Gaurav"
+const repoCount = 10
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Dunia madarchod hai')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
+
+const newStringOne = "   Gaurav    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://insta.com/facebook%20whatsapp"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('pretty'))
+
+console.log(gameName.split('-'));
